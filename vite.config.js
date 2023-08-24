6b1480a9d4cd4eeb543871cb9768f888417c254e
Leonardo Rosas <leonrosant92@gmail.com>
2023-08-23 18:12:25 -0600

    :bug: Fix assets

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from "vite-plugin-static-copy";
 import react from '@vitejs/plugin-react'
+import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: `${path.resolve(__dirname, "./src/assets")}/[!.]*`, // 1️⃣
+          dest: "./assets", // 2️⃣
+        },
+      ],
+    }),
+  ],
 })
