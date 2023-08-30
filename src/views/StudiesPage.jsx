cf9c04d0bb7fa335d573bca2e918e356459fc047
davidOlea <luis.david.olea.mtz@gmail.com>
2023-08-29 18:01:48 -0600

    use state API

--- src/views/StudiesPage.jsx
+++ src/views/StudiesPage.jsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
 import AccordionEstudios from "../components/AccordionEstudios";
-import {categorias} from "./../modules/estudiosData"
 export default function StudiesPage() {
-  
+
+  const [categorias, setCategorias] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch("https://apilcp.onrender.com/api/estudios");
+      const cat = await response.json();
+      console.log(cat);
+      setCategorias(cat);
+    }
+    getData();
+
+  }, [])
+
   return (
     <>
       <div className="py-2 d-flex flex-column align-items-center">
